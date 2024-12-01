f6dd0b0bebea704b6875303f649c4be8c507ccc9
g0ldexperiencerequiem <pborisenko2006@yandex.ru>
2024-12-01 16:22:22 +0300

    я люблю всеволода

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView
+from authentication.views import RegisterView, LoginView, LogoutView
+
 
 urlpatterns = [
-    path('v1/authentication/register/', RegisterView.as_view(), name='register'),
-    path('v1/authentication/login/', LoginView.as_view(), name='login'),
-    path('v1/authentication/logout/', LogoutView.as_view(), name='logout'),
+    path("v1/authentication/register/", RegisterView.as_view(), name='register'),
+    path("v1/authentication/login/", LoginView.as_view(), name='login'),
+    path("v1/authentication/logout/", LogoutView.as_view(), name='logout'),
 ]
