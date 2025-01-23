3e27dee2526e31c29b3d57a753bb0420e7e493b7
g0ldexperiencerequiem <pborisenko2006@yandex.ru>
2025-01-23 15:12:35 +0300

    add pipiska

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("v1/authentication/", include("djoser.urls")),
     path("v1/authentication/", include("djoser.urls.jwt")),
+    path("accounts/", include("allauth.urls")),
 ]
